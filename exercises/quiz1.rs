26344d20f7f8644fc3545f0745f9943c728a8b43
UselessPosen <tgw768@gmail.com>
2024-05-14 10:46:38 +0800

    exercises1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_numb: i32) -> i32 {
+    if  apple_numb < 41 {
+        apple_numb * 2
+    } else {
+        apple_numb
+    }
+}
 
 // Don't modify this function!
 #[test]
